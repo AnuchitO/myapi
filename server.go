56e67b2bf2ca63735d486d350c8df559d2ef584b
AnuchitO <anuchitp@thoughtworks.com>
2019-02-07 16:12:37 +0700

    native http go simple api

--- server.go
+++ server.go
@@ -1,21 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("hello handler")
-	hello := []byte(`{"name": "anuchito"}`)
-	w.Write(hello)
+type Todo struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+// var todos = make(map[string]Todo)
+var todos []Todo
+
+func todosHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("METHOD => ", req.Method)
+	method := req.Method
+
+	if method == "GET" {
+		resp, err := json.Marshal(todos)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("error na" + err.Error()))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(resp)
+	}
+
+	if method == "POST" {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "error : %v", err)
+			return
+		}
+
+		var item Todo
+		err = json.Unmarshal(body, &item)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("json unmarshal error" + err.Error()))
+			return
+		}
+
+		todos = append(todos, item)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello " + method + " created todos"))
+	}
 }
 
 func main() {
-	fmt.Println("start...")
+	http.HandleFunc("/todos", todosHandler)
 
-	http.HandleFunc("/", helloHandler)
-	http.ListenAndServe(":1234", nil)
-
-	fmt.Println("end!")
+	log.Fatal(http.ListenAndServe(":1234", nil))
 }
