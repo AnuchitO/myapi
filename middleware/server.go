f902cb221a4faf9e963b25af1b548b00a4da3d92
AnuchitO <anuchitp@thoughtworks.com>
2019-02-08 16:50:33 +0700

    example middleware

---
+++ middleware/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func helloHandler(c *gin.Context) {
+	log.Println("in hello handler")
+	c.JSON(http.StatusOK, gin.H{"status": "hello"})
+}
+
+func loginMiddleware(c *gin.Context) {
+	log.Println("starting middleware")
+	authKey := c.GetHeader("Authorization")
+	if authKey != "token123" {
+		c.JSON(http.StatusUnauthorized, "Unauthorized")
+		c.Abort()
+		return
+	}
+
+	c.Next()
+
+	log.Println("ending middleware")
+}
+
+func main() {
+	r := gin.Default()
+
+	r.Use(loginMiddleware)
+
+	r.POST("/hello", helloHandler)
+	r.Run(":1234")
+}
